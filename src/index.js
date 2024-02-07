75687b90662067215d0f49552396071ef145650d
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-07 17:42:23 +0200

    add toast

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
   </Provider>
 );
--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,32 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { Zoom, toast } from 'react-toastify';
 
 const instanse = axios.create({
   baseURL: 'https://65bd5dd5b51f9b29e9335ab7.mockapi.io',
 });
+
+const notify = () =>
+  (toastId = toast.info('... Please wait a new contact is added', {
+    autoClose: false,
+    transition: Zoom,
+  }));
+const update = data =>
+  toast.update(toastId, {
+    render: `Added a new contact: ${data.name}`,
+    type: 'success',
+    autoClose: 2000,
+    transition: Zoom,
+  });
+
+let toastId = null;
 
 export const fetchAll = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
       const { data } = await instanse.get('/contacts');
+      toast.info(`You have ${data.length} contacts`, { transition: Zoom });
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -21,7 +38,9 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (newContact, thunkAPI) => {
     try {
+      notify();
       const { data } = await instanse.post('/contacts', newContact);
+      update(data);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
